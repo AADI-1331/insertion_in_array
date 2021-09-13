b91698deecfdf6f7bf393c57dc6ca64f83c760d7
Aaditya Jaiswal <78849620+AADI-1331@users.noreply.github.com>
2021-09-13 23:39:59 +0530

    Create insertion_in_array.c

---
+++ insertion_in_array.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void display(int arr[], int n)
+{
+    //traversal
+    for(int i=0;i<n;i++)
+    {
+      printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int indexinsertion( int arr[], int size,int element,int capacity,int index)
+{
+    if(size >= capacity)
+    {
+      return -1;
+    }
+    for(int i = size-1 ; i >= index ; i --)
+    {
+        arr[i+1] = arr[i];
+    }
+    arr[index] = element;
+    return 1;
+}
+
+int main()
+{
+    int arr[100]= {1, 2, 3, 4};
+    int size=4,element=5,index=3;
+    display(arr,size);
+    indexinsertion(arr, size,element,100,index);
+    size+=1;
+    display(arr,size);
+
+
+    return 0;
+}
